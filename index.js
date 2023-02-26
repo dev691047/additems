73f2b85e5833a3a80bdef61c05cdac299cef3b06
kumardev <dev691047@gmail.com>
2023-02-26 19:54:41 +0530

    added edit

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
 const form=document.getElementById("form");
 const itemList=document.getElementById("container");
 const totalValue=document.getElementById("totalvalue");
-var edit_id=null;
+let edit_id=null;
+let array=[];
 
 function displayItem(data){
     const len=data.length;
-    for(let i=0;i<len;i++){
-      var li=document.createElement("li");
-      var button=document.createElement("button");
-      var editbutton=document.createElement("button");
+      itemList.innerHTML='';
+      for(let i=0;i<len;i++){
+      let li=document.createElement("li");
+      let button=document.createElement("button");
+      let editbutton=document.createElement("button");
       button.classList="btn";
       editbutton.classList="edtbtn";
       li.classList="list";
-      li.idList=data[i]._id;
+    //   li.idList=data[i]._id;
+      li.setAttribute("id",data[i]._id);
       button.appendChild(document.createTextNode("delete"));
       editbutton.appendChild(document.createTextNode("edit"));
       li.appendChild(button);
       li.appendChild(editbutton);
-      var itemList=document.getElementById("container");
+      let itemList=document.getElementById("container");
       li.appendChild(document.createTextNode(data[i].name+" "+data[i].price));
       itemList.appendChild(li);
-
+     
     }
 }
 
@@ -35,86 +38,115 @@ function displayCost(data){
     totalValue.innerHTML=total;
 }
 
-window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/41f49858018b4c679665c9ef33747c08/products")
-    .then((res)=>{
-        displayItem(res.data);
-        displayCost(res.data);
-        console.log(res.data)
-}).catch((err=>console.log(err)));
+window.addEventListener("DOMContentLoaded",async()=>{
+ try{ 
+   const res=await axios.get("https://crudcrud.com/api/0bc3688ab93441fab763ce147ba65c9b/products")
+   array=res.data;
+    displayItem(array);
+    displayCost(array);
 
+}
+catch{(err)=> console.log(err)}
 })
 
-
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    var name=document.getElementById("name").value;
-    var price=document.getElementById("sp").value;
+// form submit
+form.addEventListener("submit",async (e)=>{
+  
+    try{
+        e.preventDefault();
+        console.log({edit_id});
+        var name=document.getElementById("name").value;
+        var price=document.getElementById("sp").value;
+        
+        const obj={
+            name,
+            price
+        }  
     
-    const obj={
-        name,
-        price
-    } 
-    if(edit_id===null){
-        axios.post(`https://crudcrud.com/api/41f49858018b4c679665c9ef33747c08/products`,obj)
-        .then((res)=>{
-            console.log(res.data);
-            document.location.reload();
+        if(edit_id===null){     
+                const res= await axios.post(`https://crudcrud.com/api/0bc3688ab93441fab763ce147ba65c9b/products`,obj)
+                array.push(res.data);
+                displayItem(array);
+                displayCost(array);  
+                document.getElementById("name").value="";
+                document.getElementById("sp").value="";  
+                    
+        }
+
+        else{
+                console.log(edit_id);
+                const res=await axios.put(`https://crudcrud.com/api/0bc3688ab93441fab763ce147ba65c9b/products/${edit_id}`,obj);
+                let newArray = [];
+                console.log({edit_id});
+
+                newArray = array.map(val=>{
+                    if(val._id==edit_id){
+                     val.name = obj.name;
+                     val.price = obj.price;   
+                    }
+                    return val;
+                });
+
+                console.log({newArray,array})
+                array = newArray;
+                displayItem(array);
+                displayCost(array);               
+                edit_id=null;
+                console.log({edit_id});
+                document.getElementById("name").value="";
+                document.getElementById("sp").value="";    
+                
             
-        }).catch((err)=>console.log(err));
+        }  
     }
-    else{
-        axios.put(`https://crudcrud.com/api/41f49858018b4c679665c9ef33747c08/products/${edit_id}`,obj)
-        .then((res)=>{
-            document.location.reload();
-            console.log(res.data);
-            if(edit_id!=null){
-                edit_id==null;
-            }
-            
-        }).catch((err)=>console.log(err));
-        console.log(edit_id);
-    }   
+    catch{(err)=>console.log(err)};
+    
 })
 
 
 
 // delete
-itemList.addEventListener("click",function(e){
+itemList.addEventListener("click",async(e)=>{
+try{
+    if(e.target.classList.contains('btn')){
+        if(confirm('are u sure??')){   
+             var li =e.target.parentElement;
+             var id=e.target.parentElement.id;
+             console.log(id);
+             
+                 const res= await axios({
+                  method:'delete',
+                  url:`https://crudcrud.com/api/0bc3688ab93441fab763ce147ba65c9b/products/${id}`,
+                   })
+                   console.log(res);
+                   itemList.removeChild(li);
+                }
+            }
+            
+}
+catch{err=>console.log(err)};
 
-  if(e.target.classList.contains('btn')){
-      if(confirm('are u sure??')){
-          
-           var li =e.target.parentElement;
-           var id=e.target.parentElement.idList;
-           console.log(id);
-           axios({
-            method:'delete',
-            url:`https://crudcrud.com/api/41f49858018b4c679665c9ef33747c08/products/${id}`,
-           })
-           .then(res=>{
-              document.location.reload(); 
-            console.log(res)})
-           .catch(err=>console.log(err));    
-           itemList.removeChild(li);
-          } }
 })
 
 
 // edit the list
-itemList.addEventListener("click",function(e){
-    e.preventDefault();
-    if(e.target.classList.contains('edtbtn')){
-             var id=e.target.parentElement.idList;
-             edit_id=id;
-             axios({
+itemList.addEventListener("click",async(e)=>{
+   
+    try{
+        e.preventDefault();
+        if(e.target.classList.contains('edtbtn')){
+            var id=e.target.parentElement.id;
+            edit_id=id;
+            console.log("working")
+           
+               const res=await axios({
                    method:'get',
-                   url:`https://crudcrud.com/api/41f49858018b4c679665c9ef33747c08/products/${id}`,
+                   url:`https://crudcrud.com/api/0bc3688ab93441fab763ce147ba65c9b/products/${id}`,
                 })
-                .then((res)=>{
-                    document.getElementById("name").value=res.data.name;
-                     document.getElementById("sp").value=res.data.price;
-                })
-                .catch((err)=>console.log(err));          
-    }  
+               document.getElementById("name").value=res.data.name;
+               document.getElementById("sp").value=res.data.price;                             
+   }
+    }
+    catch{er=>console.log(er)}
+   
   }) 
